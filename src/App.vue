e8a56019510e16fb65a9116c21c042e9dfb6d495
tatashir <tshirotai9683@gmail.com>
2024-05-17 17:32:18 +0900

    add 996jpg

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/996.jpg" />
+    <h1>Welcome to my page</h1>
+    <p>This is the index page of my page hosted on GitHub Pages.</p>
+    <!-- <p>this is my bike.</p> -->
+    <div>
+      <!-- <img alt="Vue logo" src="./assets/996.jpg" /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+img {
+  width: 300px;
+  height: auto;
+  /* border-radius: 50%; */
+  margin: 0 auto;
+  display: block;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
